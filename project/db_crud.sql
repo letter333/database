8e139b00b34d41cdb84c1eaeaf047df9bd6123dc
letter333 <68866597+letter333@users.noreply.github.com>
2022-09-04 00:15:21 +0900

    db crud합본

--- project/db_crud.sql
+++ project/db_crud.sql
@@ -1,39 +1,62 @@
--- ¼ºº° Å×ÀÌºí
+-- ì„±ë³„ í…Œì´ë¸”
 create sequence gender_num;
 
-insert into gender(gender_num, gender_type) values (gender_num.nextval, '³²ÀÚ');
-insert into gender(gender_num, gender_type) values (gender_num.nextval, '¿©ÀÚ');
-insert into gender(gender_num, gender_type) values (gender_num.nextval, 'ºñ°ø°³');
-insert into gender(gender_num, gender_type) values (gender_num.nextval, 'Áß¼º');
+insert into gender(gender_num, gender_type) values (gender_num.nextval, 'ë‚¨ì');
+insert into gender(gender_num, gender_type) values (gender_num.nextval, 'ì—¬ì');
+insert into gender(gender_num, gender_type) values (gender_num.nextval, 'ë¹„ê³µê°œ');
+insert into gender(gender_num, gender_type) values (gender_num.nextval, 'ì¤‘ì„±');
 
-update gender set gender_type='unknown' where gender_type='ºñ°ø°³';
-delete from gender where gender_type='Áß¼º';
+update gender set gender_type='unknown' where gender_type='ë¹„ê³µê°œ';
+delete from gender where gender_type='ì¤‘ì„±';
 
 
--- ¼±È£ºĞ¾ß Å×ÀÌºí
+-- ì„ í˜¸ë¶„ì•¼ í…Œì´ë¸”
 create sequence prefer_num;
-insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'µ¿¹°');
-insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'µå¶ó¸¶');
-insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, '¿©Çà');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ë™ë¬¼');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ë“œë¼ë§ˆ');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ì—¬í–‰');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ê²Œì„');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ë°©íƒˆì¶œ');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ë“±ì‚°');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'í˜¸í¡');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ë¯¼íŠ¸ì´ˆì½”');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ì½”ë”©');
+insert into prefer(prefer_num, prefer_type) values (prefer_num.nextval, 'ë°±ì¤€');
 
-update prefer set prefer_type='Ä·ÇÎ' where prefer_type='¿©Çà';
-delete from prefer where prefer_type='µå¶ó¸¶';
+update prefer set prefer_type='ìº í•‘' where prefer_type='ì—¬í–‰';
+update prefer set prefer_type='í—¬ìŠ¤' where prefer_num=4;
+delete from prefer where prefer_type='ë“œë¼ë§ˆ';
 
--- È¸¿øÁ¤º¸ Å×ÀÌºí
+-- íšŒì›ì •ë³´ í…Œì´ë¸”
 create sequence user_num;
 insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
-values (user_num.nextval, 'letter333', 'password1', 'letter',  '¿À¼¼·æ',  to_date('1998-02-17', 'YYYY-MM-DD'), 'seryong12@gmail.com', '010-3194-0217', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 1, 1);
+values (user_num.nextval, 'letter333', 'password1', 'letter',  'ì˜¤ì„¸ë£¡',  to_date('1998-02-17', 'YYYY-MM-DD'), 'seryong12@gmail.com', '010-3194-0217', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 1, 1);
 
 insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
-values (user_num.nextval, 'jihop379', 'pa5sword2', 'errorj',  '¹ÚÁöÈ£',  to_date('1995-01-14', 'YYYY-MM-DD'), 'jihop379@gmail.com', '010-1234-1234', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 2, 1);
+values (user_num.nextval, 'jihop379', 'pa5sword2', 'errorj',  'ë°•ì§€í˜¸',  to_date('1995-01-14', 'YYYY-MM-DD'), 'jihop379@gmail.com', '010-1234-1234', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 2, 1);
 
 insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
-values (user_num.nextval, 'igodda0654', 'pAssword3', 'sik',  'ÀÌ¹ü½Ä',  to_date('1990-01-01', 'YYYY-MM-DD'), 'igodda0654@gmail.com', '010-4321-4312', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 1, 3);
+values (user_num.nextval, 'igodda0654', 'pAssword3', 'sik',  'ì´ë²”ì‹',  to_date('1990-01-01', 'YYYY-MM-DD'), 'igodda0654@gmail.com', '010-4321-4312', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 1, 3);
+
+insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
+values (user_num.nextval, 'mong', 'mong_pass', 'mong_nick',  'ìˆ­ì´',  to_date('2000-01-01', 'YYYY-MM-DD'), 'mong@mail.com', '010-0000-0001', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 1, 5);
+
+insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
+values (user_num.nextval, 'cowww', 'cowww_pass', 'cow_nick',  'ë§ë‘ì¹´ìš°',  to_date('2000-01-02', 'YYYY-MM-DD'), 'cowww@mail.com', '010-0000-0002', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 3, 4);
+
+insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
+values (user_num.nextval, 'dog', 'dog_pass', 'dog_nick',  'ë©ì´',  to_date('2000-01-03', 'YYYY-MM-DD'), 'meong@mail.com', '010-0000-0003', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 1, 7);
+
+insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
+values (user_num.nextval, 'parkjiho14', 'ppp1234', 'mint',  'ë°•ì§€í˜¸',  to_date('2000-01-03', 'YYYY-MM-DD'), 'jihop379@mail.com', '010-0000-0004', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 2, 8);
+
+insert into user_info(user_num, user_id, user_password, user_nickname, user_name, user_birth, user_email, user_phonenum, user_regdate, user_profile_name, user_profile_path, user_gender_num, user_prefer_num)
+values (user_num.nextval, 'hacker', 'strongbeom', 'hacker19',  'í•´ì»¤ë²”',  to_date('2000-08-18', 'YYYY-MM-DD'), 'dark_hacker19@mail.com', '010-7808-1818', to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), null, null, 3, 9);
 
 update user_info set user_password='drowssap' where user_password='pAssword3';
 delete from user_info where user_id='letter333';
 
--- °Ô½Ã±Û Å×ÀÌºí
+-- ê²Œì‹œê¸€ í…Œì´ë¸”
 create sequence post_num;
 insert into post(post_num, post_created_at, post_user_num, post_title, post_content, post_writer_position, post_comment_count)
 values (post_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 2, 'test title1', 'test content', null, 0);
@@ -44,17 +67,43 @@ values (post_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 3, 'test ti
 insert into post(post_num, post_created_at, post_user_num, post_title, post_content, post_writer_position, post_comment_count)
 values (post_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 2, 'test title3', 'test content33', null, 6);
 
-update post set post_writer_position='´ë±¸' where post_user_num=2;
+insert into post(post_num, post_created_at, post_user_num, post_title, post_content, post_writer_position, post_comment_count)
+values (post_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 4, 'post_mong', 'mongmongmong', null, 3);
+
+insert into post(post_num, post_created_at, post_user_num, post_title, post_content, post_writer_position, post_comment_count)
+values (post_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 5, 'post_cowww', 'cowwwwwwwwwww', null, 1);
+
+insert into post(post_num, post_created_at, post_user_num, post_title, post_content, post_writer_position, post_comment_count)
+values (post_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 6, 'post_dog', 'meong post content', null, 2);
+
+insert into post(post_num, post_created_at, post_user_num, post_title, post_content, post_writer_position, post_comment_count)
+values (post_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 2, 'popopopcatcorn', 'íŒìº£ì€ ê·€ì—¬ì›Œ', null, 9);
+
+update post set post_writer_position='ëŒ€êµ¬' where post_user_num=2;
 delete from post where post_comment_count=0;
 
--- ´ñ±Û Å×ÀÌºí
+-- ëŒ“ê¸€ í…Œì´ë¸”
 create sequence comment_num;
 insert into comments(comment_num, comment_created_at, comment_user_num, comment_post_num, comment_content)
 values (comment_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 2, 2, 'comment1');
+
 insert into comments(comment_num, comment_created_at, comment_user_num, comment_post_num, comment_content)
 values (comment_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 3, 3, 'comment2');
+
 insert into comments(comment_num, comment_created_at, comment_user_num, comment_post_num, comment_content)
 values (comment_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 3, 2, 'comment33');
+
+insert into comments(comment_num, comment_created_at, comment_user_num, comment_post_num, comment_content)
+values (comment_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 4, 5, 'kkkkkkk');
+
+insert into comments(comment_num, comment_created_at, comment_user_num, comment_post_num, comment_content)
+values (comment_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 6, 4, 'really?');
+
+insert into comments(comment_num, comment_created_at, comment_user_num, comment_post_num, comment_content)
+values (comment_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 6, 5, 'oh my cat!');
+
+insert into comments(comment_num, comment_created_at, comment_user_num, comment_post_num, comment_content)
+values (comment_num.nextval, to_char(sysdate, 'YYYY-MM-DD HH24:MI:SS'), 4, 7, 'ìš°ì™€ ê·€ì—½ë‹¤');
 
 update comments set comment_content='update comment' where comment_content='comment33';
 delete from comments where comment_num = 1;
